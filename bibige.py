4f01280ae4a9dd8274dad95b33360a77bd9aa9c5
Valera <95861037+letsho@users.noreply.github.com>
2023-01-18 18:22:23 +0200

    Create bibige.py

---
+++ bibige.py
@@ -0,0 +1,106 @@
+def func1(arg1, arg2):
+    var5 = class2()
+    for var6 in func4(arg2, arg1):
+        var5.func3(var6, arg2)
+    var51 = func6(arg1, arg2)
+    var52 = arg2 + -169
+    var53 = 963067692 & var51
+    var54 = var53 ^ var51 - var53 - 241167973
+    if arg1 < var53:
+        var55 = (arg2 + arg1) - var54
+    else:
+        var55 = arg2 - (var53 - var51)
+    var56 = var54 ^ var51 - var54 | -409
+    var57 = arg2 & (arg2 + var52 + arg2)
+    var58 = var52 - var51
+    var59 = ((257 + 1596150724) & var54) & -1349098271
+    var60 = -1206413923 ^ (-344924028 | var56 | 715609278)
+    var61 = (var56 & var52 - -532) & var51
+    var62 = var56 | 421 ^ var60
+    var63 = 957070765 ^ arg1 - var51 | arg1
+    var64 = var58 + var62
+    result = (((var56 + arg1 + var64 ^ ((var52 & var56) + (arg2 & var59) - var51)) - var58) | var52) ^ var57 & arg2
+    return result
+def func6(arg35, arg36):
+    var37 = 0
+    for var50 in (7 + (var37 & var37) | (var37 ^ (arg35 + (arg36 - arg36 ^ arg35)) & i ^ -1) ^ arg36 & 0 for i in func7(arg36, 8)):
+        var37 += arg35 & 4
+    return var37
+def func7(arg38, arg39):
+    var40 = -539 + arg39
+    yield var40
+    var41 = (arg39 + arg39) ^ arg39
+    yield var41
+    var42 = -1167903140 & (arg38 & -1211728768) + arg38
+    yield var42
+    var43 = var40 + (arg38 + 392) ^ var41
+    yield var43
+    var44 = (arg39 | arg38 & arg39) & arg39
+    yield var44
+    var45 = (var42 + -755 & var44) & -332
+    yield var45
+    var46 = -1427339644 + arg39 & var43
+    yield var46
+    var47 = (var42 | var45 ^ var46) + 637
+    yield var47
+    var48 = var41 - arg38
+    yield var48
+    var49 = var47 | arg39 - var41
+    yield var49
+def func5(arg9, arg10):
+    var11 = (-796 | -649872598 - -627233720) ^ 906852524
+    var12 = (529 - var11) - 656983299 - 664276511
+    var13 = arg10 | 165
+    var14 = 199965874 - (var12 & (var12 | arg10))
+    if var14 < arg9:
+        var15 = var12 | arg9 ^ var12
+    else:
+        var15 = var11 ^ (var11 & var12 - -856)
+    var16 = arg9 & (-143518367 | (-2060172819 | var14))
+    var17 = (var13 & 1526690743) & var14 - var16
+    var18 = var11 + var13
+    var19 = var13 ^ var12 + (var12 + var12)
+    var20 = (var17 - arg9 - var18) | var14
+    var21 = (var17 & var13 + var12) & var13
+    var22 = var21 & var12
+    if arg10 < arg9:
+        var23 = (var16 - var17 - var22) | var17
+    else:
+        var23 = var16 ^ var21
+    result = (-710398200 ^ var21 + ((2000051590 ^ (((var19 & arg10) ^ var12) + var11 ^ var17)) | var16) & var22) + var20 & var16
+    return result
+def func4(arg7, arg8):
+    var24 = func5(arg7, -590821419)
+    yield var24
+    var25 = -249 & arg7 ^ -1468350355 | 918
+    yield var25
+    var26 = -368 | var25
+    yield var26
+    var27 = arg8 ^ var26 & -625 & arg7
+    yield var27
+    var28 = arg8 + arg8
+    yield var28
+    var29 = arg7 & arg7 & (326451229 + var25)
+    yield var29
+    var30 = (var28 ^ arg8) | var26 + var28
+    yield var30
+    var31 = var30 | var30
+    yield var31
+    var32 = ((var25 - var31) - -24) | 68568538
+    yield var32
+    var33 = var32 ^ (1759315500 | arg7) | var30
+    yield var33
+    var34 = var33 & var33
+    yield var34
+class class2(object):
+    def func3(self, arg3, arg4):
+        result = -1605582373 ^ arg4 + (1 | arg4) + 278669024 | arg4 - arg3
+        return result
+if __name__ == "__main__":
+    print 'prog_size: 5'
+    print 'func_number: 8'
+    print 'arg_number: 65'
+    for i in xrange(25000):
+        x = 5
+        x = func1(x, i)
+        print x,
